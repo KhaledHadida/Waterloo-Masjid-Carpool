7935dd2b623e98203b4f972cdf7d2820c9fb482c
Majed Hadida <majedhadida@hotmail.com>
2023-09-23 22:26:23 -0400

    contact page

--- app/contact/page.js
+++ app/contact/page.js
@@ -6,7 +6,22 @@ const ContactPage = () => {
     return (
         <div>
             <Header />
-            <div>Contact Page</div>
+            <div class="flex justify-center items-center p-10 text-lg">
+                <p>Get in touch with us.</p>
+            </div>
+            <div class="flex justify-center items-center">
+                <div class="p-10 text-lg bg-gray-300 w-1/2">
+                    <label for="name" class="pr-5">Name</label>
+                    <input type="text" id="name" name="name" autoComplete='off' /><br /><br />
+                    <label for="email" class="pr-5">Email</label>
+                    <input type="text" id="email" name="email" autoComplete='off' /><br /><br />
+                    <label for="phone" class="pr-5">Phone</label>
+                    <input type="text" id="phone" name="phone" autoComplete='off' /><br /><br />
+                    <label for="msg" class="pr-5 block">Message</label>
+                    <input type="text" id="msg" name="msg" autoComplete='off' class="w-10/12 h-56" /><br /><br />
+                </div>
+            </div>
+
             <Footer />
         </div>
 
